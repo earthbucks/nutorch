8e309c660374ff11e8a99d24b82a2ebe31009fc9
Ryan X. Charles <ryan@ryanxcharles.com>
2025-06-26 18:21:52 -0700

    sin

--- rs/src/main.rs
+++ rs/src/main.rs
@@ -7,6 +7,10 @@ fn linspace(start: f64, end: f64, steps: i64) -> Tensor {
     Tensor::linspace(start, end, steps, (Kind::Float, Device::Cpu))
 }
 
+fn sin(tensor: &Tensor) -> Tensor {
+    tensor.sin()
+}
+
 fn main() {
     let t = Tensor::from_slice(&[3, 1, 4, 1, 5]);
     let t = t * 2;
