2299593ed54bcda37f758201cfa4900c9346a09b
Ryan X. Charles <ryan@ryanxcharles.com>
2025-06-30 13:56:24 -0700

    node-custom-server -> termplot

--- ts/termplot/vite.config.ts
+++ ts/termplot/vite.config.ts
@@ -3,6 +3,13 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig({
-  plugins: [reactRouter(), tsconfigPaths(), tailwindcss()],
-});
+export default defineConfig(({ isSsrBuild }) => ({
+  build: {
+    rollupOptions: isSsrBuild
+      ? {
+          input: "./server/app.ts",
+        }
+      : undefined,
+  },
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+}));
