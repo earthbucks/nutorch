d8d53d81ecd3ea6527b456408aae9ef36535059f
Ryan X. Charles <ryan@ryanxcharles.com>
2025-06-30 13:09:10 -0700

    create express app

--- ts/termplot/server/server.ts
+++ ts/termplot/server/server.ts
@@ -1,7 +1,11 @@
 import { createRequestHandler } from "@react-router/express";
 import { type ServerBuild } from "react-router";
 import compression from "compression";
-import express, { type Request, type Response, type NextFunction } from "express";
+import express, {
+  type Request,
+  type Response,
+  type NextFunction,
+} from "express";
 import morgan from "morgan";
 
 const viteDevServer =
@@ -13,3 +17,10 @@ const viteDevServer =
           // server: { middlewareMode: true, watch: { usePolling: true } },
         }),
       );
+
+const app = express();
+
+app.use(compression());
+
+// http://expressjs.com/en/advanced/best-practice-security.html#at-a-minimum-disable-x-powered-by-header
+app.disable("x-powered-by");
